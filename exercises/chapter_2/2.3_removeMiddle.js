21d92a90dcb74d69fa328fd914852eb9fbcff859
vgtmhl <vgtmhl@gmail.com>
2022-05-22 15:13:42 +0200

    2.3 remove middle node from list

--- exercises/chapter_2/2.3_removeMiddle.js
+++ exercises/chapter_2/2.3_removeMiddle.js
@@ -10,5 +10,17 @@ import { getRandomList } from './../../structures/linkedList.js';
  * What I can do, though, is to copy the next node into current (both value and next), and then remove the next node.
  */
 
-const list = getRandomList()
-list.printList({ compact: true })
+function removeNode(list, node) {
+
+    // check that this is not the head nor the tail
+    // I can actually use === as I'm comparing the memory addresses of these 2:D
+    if (list.head === node || !node.next) {
+        return;
+    }
+
+    // copy the next node into the current one, unlink the next node
+    node.value = node.next.value;
+    node.next = node.next.next;
+    node.next.next = null;
+}
+
